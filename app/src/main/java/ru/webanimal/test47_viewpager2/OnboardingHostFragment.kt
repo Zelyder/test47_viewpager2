addfc4f772e1db962abaf5bbed2bbf7be19712f2
Sergio <webanimal.group@gmail.com>
2020-12-09 13:57:04 +0300

    Pager indicator completed. All is done.

--- app/src/main/java/ru/webanimal/test47_viewpager2/OnboardingHostFragment.kt
+++ app/src/main/java/ru/webanimal/test47_viewpager2/OnboardingHostFragment.kt
@@ -2,11 +2,13 @@ package ru.webanimal.test47_viewpager2
 
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
 
@@ -15,6 +17,8 @@ class OnboardingHostFragment : Fragment() {
     private var exitListener: ExitClickListener? = null
     private var pager: ViewPager2? = null
     private var skipButton: Button? = null
+    private var indicatorsLayout: LinearLayout? = null
+    private var indicators: MutableList<ImageView> = mutableListOf()
     private var pagesCount = 0
     private var currentPage = 0
     
@@ -45,14 +49,8 @@ class OnboardingHostFragment : Fragment() {
                 super.onPageSelected(position)
         
                 currentPage = position
-                val textResId = if (currentPage >= (pagesCount - 1)) {
-                    R.string.onboarding_page_button_close
-            
-                } else {
-                    R.string.onboarding_page_button_skip
-                }
-                skipButton?.text = getString(textResId)
-                Log.d("TAG", "TEST::onPageSelected pos:$currentPage")
+                updateIndicators(currentPage)
+                updateSkipButton(currentPage)
             }
         })
         
@@ -62,6 +60,57 @@ class OnboardingHostFragment : Fragment() {
                 else -> pager?.setCurrentItem(pagesCount - 1, true)
             }
         }}
+    
+        indicatorsLayout = view.findViewById(R.id.layIndicators)
+        initIndicators(indicatorsLayout)
+    }
+    
+    private fun initIndicators(layout: LinearLayout?) {
+        layout?.let {
+            val context = it.context
+            it.removeAllViews()
+            val params = LinearLayout.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+            params.setMargins(
+                    context.dpToPx(4f).toInt(),
+                    context.dpToPx(4f).toInt(),
+                    context.dpToPx(4f).toInt(),
+                    context.dpToPx(4f).toInt()
+            )
+            for (i in 0 until pagesCount) {
+                val indicator = ImageView(context).apply {
+                    layoutParams = params
+                    setImageResource(R.drawable.shape_pager_indicator_default)
+                }
+                indicators.add(indicator)
+                layout.addView(indicator, i)
+            }
+        }
+    }
+    
+    private fun updateIndicators(currentPage: Int) {
+        var drawableResId = 0
+        for (i in indicators.indices) {
+            drawableResId = if (i == currentPage) {
+                R.drawable.shape_pager_indicator_selected
+        
+            } else {
+                R.drawable.shape_pager_indicator_default
+            }
+            indicators[i].setImageResource(drawableResId)
+        }
+    }
+    
+    private fun updateSkipButton(currentPage: Int) {
+        val textResId = if (currentPage >= (pagesCount - 1)) {
+            R.string.onboarding_page_button_close
+        
+        } else {
+            R.string.onboarding_page_button_skip
+        }
+        skipButton?.text = getString(textResId)
     }
     
     private fun preparePages(): List<Page> {
@@ -87,6 +136,7 @@ class OnboardingHostFragment : Fragment() {
         exitListener = null
         pager = null
         skipButton = null
+        indicatorsLayout = null
         
         super.onDetach()
     }
@@ -99,3 +149,9 @@ class OnboardingHostFragment : Fragment() {
         fun create(): OnboardingHostFragment = OnboardingHostFragment()
     }
 }
+
+private fun Context.dpToPx(dp: Float) = TypedValue.applyDimension(
+        TypedValue.COMPLEX_UNIT_DIP,
+        dp,
+        this.resources.displayMetrics
+)
